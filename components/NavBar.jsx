5a1c98f9e3cc774c4363d3957080d7c043834840
Clavien317 <Claviennambinina511@gmail.com>
2024-07-09 10:00:17 +0300

    first commit

---
+++ components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <>
+        <header>
+            <div className="container">
+                <div className="logo">
+                    <Image src=""></Image> #INNOV-T </div>
+                    <nav>
+                        <ul>
+                            <li><a href="">Accueil</a></li>
+                            <li><a href="">Pourquoi INNOV-T</a></li>
+                            <li><a href="">Nos service</a></li>
+                            <li><a href="">Equipes</a></li>
+                            <li><button><a href="">Postulez?</a></button></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </nav>
+            </div>
+        </header>
+    </>
+  )
+}
+
+export default NavBar
---
+++ components/Slide1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Slide1() {
+  return (
+    <>
+        <main>
+            <div className="contenu-slide1">
+                <h2>Slide1</h2>
+            </div>
+        </main>
+    </>
+  )
+}
+
+export default Slide1
